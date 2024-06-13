b38f3f437863ef58d4497c085a45dae9ba3a36df
demianmedich <demianmedich@gmail.com>
2024-06-13 15:42:02 +0900

    Initial codes

---
+++ similarity_checker.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+
+
+class SimilarityChecker:
+
+    def __init__(self):
+        self.lhs: str = ""
+        self.rhs: str = ""
+
+    def set_strings(self, lhs: str, rhs: str):
+        self.lhs = lhs
+        self.rhs = rhs
+
+    def total_score(self) -> float:
+        return self.check_length() + self.check_letters()
+
+    def check_length(self) -> float:
+        # TODO: Implement me
+        raise NotImplementedError()
+
+    def check_letters(self) -> float:
+        # TODO: Implement me
+        raise NotImplementedError()
---
+++ test_similarity_checker.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class SimilarityCheckerTestCase(unittest.TestCase):
+    def test_something(self):
+        # TODO: Implement me
+        raise NotImplementedError()
+
+
+if __name__ == "__main__":
+    unittest.main()
