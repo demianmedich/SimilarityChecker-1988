d5af28c4f09cb2191fad5e1af2ef6fdc058c0dd4
demianmedich <demianmedich@gmail.com>
2024-06-13 15:56:49 +0900

    [refactor] change TC

--- test_similarity_checker.py
+++ test_similarity_checker.py
@@ -4,15 +4,18 @@ from similarity_checker import SimilarityChecker
 
 
 class SimilarityCheckerTestCase(unittest.TestCase):
-    def test_check_letters(self):
-        checker = SimilarityChecker()
-        checker.set_strings("ASD", "DSA")
-        score = checker.check_letters()
-        self.assertEqual(score, 60.0)
 
-        checker.set_strings("A", "BB")
-        score = checker.check_letters()
-        self.assertEqual(score, 0.)
+    def setUp(self):
+        self.checker = SimilarityChecker()
+
+    def test_check_letters(self):
+        self.assert_equal_score("ASD", "DSA", 60.0)
+        self.assert_equal_score("A", "BB", 0.0)
+
+    def assert_equal_score(self, lhs: str, rhs: str, expected_score: float):
+        self.checker.set_strings(lhs, rhs)
+        score = self.checker.check_letters()
+        self.assertEqual(score, expected_score)
 
 
 if __name__ == "__main__":
